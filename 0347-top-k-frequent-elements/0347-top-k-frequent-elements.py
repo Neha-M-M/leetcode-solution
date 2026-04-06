a495e30af311462178f3d0fccacb0cb385482299
Neha-M-M <mmneha2011@gmail.com>
2026-04-06 17:21:05 +0530

    Time: 7 ms (50.32%), Space: 22.7 MB (86.85%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        heap = []
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for count, num in heap]
